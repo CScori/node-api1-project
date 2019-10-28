d4bb1dd79cc1461ee691df5a3f559c6a686f2ace
CScori <amilanotte1366@gmail.com>
2019-10-28 16:30:45 -0500

    more logic fix on user by id

--- index.js
+++ index.js
@@ -35,15 +35,16 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     // define single user id from db
     const id = req.params.id
-    //function to run info
-    db.findById(id)
-    //promise
+    (`!users/${id}`)
+    ? res 
+    .status(404)
+    .json({ message: "The user with the specified ID does not exist." })
+    :db.
+    findById(id)
     .then(() => {
         res.status(200).json(users)
     })
-    .catch(err => {
-        console.log('error', err);
-        res.status(404).json({error: "The user with the specified ID does not exist"})
+    .catch(() => {
         res.status(500).json({ error: 'The user information could not be retrieved.' });
       });
 })
