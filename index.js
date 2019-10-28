cb6839bc39d58bd91fe7182c12b04b5f70f7b036
CScori <amilanotte1366@gmail.com>
2019-10-28 16:09:05 -0500

    adds find by id get

--- index.js
+++ index.js
@@ -43,8 +43,25 @@ server.post('/api/users', (req, res) => {
         res.status(400).json()
     })
     // may possibly need to edit 400 if body info is not right
+    //also considering multiple fn for diff cases not sure
     .catch(err => {
         console.log('error', err);
         res.status(500).json({ error: 'failed to add new users to db' });
+      });
+})
+
+//get specific user by id
+server.get('/api/users/:id', (req, res) => {
+    // define single user id from db
+    const id = req.params.id
+    //function to run info
+    db.findById(id)
+    //promise
+    .then(() => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        console.log('error', err);
+        res.status(500).json({ error: 'failed to get single user from db' });
       });
 })
